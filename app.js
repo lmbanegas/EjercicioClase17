5806f565287f07a8e1a48ed08d7aba6d54ff5533
lmbanegas <lbanegas93@gmail.com>
2023-07-13 13:45:43 -0300

    Proyecto finalizado

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-
 const path = require('path');
 
+
+const app = express();
 app.use(express.static('public'));
 
+const publicPath = path.join(__dirname,'public');
+
+
+
+app.get ('/',(req,res) => {
+    res.sendFile (path.join(__dirname, '/views/home.html'));
+});
+
+app.get ('/login',(req,res) => {
+    res.sendFile (path.join(__dirname, '/views/login.html'));
+});
+
+app.get ('/register',(req,res) => { 
+        res.sendFile (path.join(__dirname, '/views/register.html'));
+});
+
+
+
+app.listen(3030, () => 
+console.log("Ok")
+);
